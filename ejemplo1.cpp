33cd461b64cd11bf3fc829412539ec98dabe650f
manuelsalcidorussell <manuelsalcido@gmail.com>
2025-10-19 22:53:27 -0700

    Actualizo configuraciones y ejemplo1.cpp funcionando

--- ejemplo1.cpp
+++ ejemplo1.cpp
@@ -1,4 +1,6 @@
 #include <iostream>  // Biblioteca para usar cout
+
 int main() {
     std::cout << "Hola mundo";  // Muestra el mensaje
     return 0;  // Indica que el programa terminó correctamente
+}
