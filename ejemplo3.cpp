8c0eb0fef4c737a30ff233b34ec38ff5357c0b94
manuelsalcidorussell <manuelsalcido@gmail.com>
2025-10-19 23:33:18 -0700

    Create ejemplo3.cpp
    prueba ejemplo 3 para github

---
+++ ejemplo3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>   // Biblioteca para usar cout
+using namespace std;  // Para evitar escribir std:: en cada línea
+
+int main() {
+    cout << "Hola, mi nombre es Manuel Salcido Russell." << endl;
+    cout << "Vivo en Culiacán, Sinaloa." << endl;
+    cout << "Estudio en la Facultad de Ingeniería Mochis." << endl;
+    cout << "Este es el ejemplo 3 del programa en C++." << endl;
+    cout << "El programa ha terminado correctamente." << endl;
+    return 0; // Indica que el programa terminó sin errores
+}
