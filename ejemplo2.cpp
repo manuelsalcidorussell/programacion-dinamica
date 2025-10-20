815341ac52350f0a16445e297ba90ac1da38af81
manuelsalcidorussell <manuelsalcido@gmail.com>
2025-10-19 23:26:30 -0700

    ejemplo 2 corre

---
+++ ejemplo2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>  // Biblioteca para usar cout
+using namespace std; // Para evitar escribir std:: en cada línea
+
+int main() {
+    cout << "Hola, mi nombre es Manuel Salcido Russell." << endl;
+    cout << "Este es el ejemplo 2 del programa en C++." << endl;
+    cout << "¡El programa ha terminado correctamente!" << endl;
+    return 0; // Indica que el programa terminó sin errores
+}
